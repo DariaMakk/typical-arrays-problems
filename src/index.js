52246619aeb995e9e2966b420a0c79eb71c4f4cc
DariaMakk <dasha-nov13@rambler.ru>
2020-10-01 10:13:22 +0300

    feat: add the solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
-
 exports.min = function min (array) {
-  return 0;
+  if (array === undefined || array[0] === undefined) return 0;
+  return Math.min.apply(null, array);
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array === undefined || array[0] === undefined) return 0; 
+  return Math.max.apply(null, array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array === undefined || array[0] === undefined) return 0;
+  let i = 0;
+  let sum = 0;
+  while (i < array.length) {
+    sum += array[i];
+    i++;
+  }
+  return sum / array.length;
 }
